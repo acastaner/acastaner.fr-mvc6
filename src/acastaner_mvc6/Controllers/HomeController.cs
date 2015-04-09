c4a6baf1a1316ab311c41985c5e31fcb5ab5a95e
Arnaud Castaner <arnaud.castaner@outlook.com>
2015-04-09 16:31:57 +0200

    Initial commit

---
+++ src/acastaner_mvc6/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace acastaner.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Title = "Accueil";
+			ViewBag.Heading = "Bienvenue";
+			ViewBag.SubHeading = "Sur mon coin d'Internet";
+			ViewBag.HeaderImage = "welcome-bg.jpg";
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Title = "A propos";
+			ViewBag.Heading = ViewBag.Title;
+			ViewBag.SubHeading = "Ce qu'il y a sous le capot";
+			ViewBag.HeaderImage = "about-bg.jpg";
+			return View();
+		}
+
+		public ActionResult Projects()
+		{
+			ViewBag.Title = "Mes projets";
+			ViewBag.Heading = ViewBag.Title;
+			ViewBag.SubHeading = "Ce qui occupe mes soirées";
+			ViewBag.HeaderImage = "post-bg.jpg";
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Title = "Contact";
+			ViewBag.Heading = ViewBag.Title;
+			ViewBag.SubHeading = "Vieille école mais fort pratique";
+			ViewBag.HeaderImage = "contact-bg.jpg";
+			return View();
+		}
+	}
+}
---
+++ src/acastaner_mvc6/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace acastaner_mvc6
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+			services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller}/{action}/{id?}",
+					defaults: new { controller = "Home", action = "Index" });
+			});
+		}
+    }
+}
